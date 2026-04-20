171d584c075c03ebd3c4a8105a04f940ed29ee86
Pachabhatla Dhanush <pachabhatladhanush@gmail.com>
2026-04-20 07:49:53 +0530

    Rename hello function to home and update response

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'Hello, world!'
+@app.route("/")
+def home():
+    return "Hello CI/CD on AWS!"
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
